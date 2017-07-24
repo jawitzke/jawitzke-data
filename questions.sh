815c80db0f86acd86e495faa802c2e7291473a8f
jawitzke <jawitzke@live.unc.edu>
2017-07-24 10:39:22 -0400

    added questions and date

--- questions.sh
+++ questions.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # Prompting Five Questions
-Echo "1. Suhh Dood"
-Echo "2. Suhh Dood"
-Echo "3. Suhh Dood"
-Echo "4. Suhh Dood"
-Echo "5. Suhh Dood"
+echo "1. Please type in the four digit year you were born in followed by [Enter]:"
+read YEAR
+echo "2. Please type in the two digit month you were born in followed by [Enter]:"
+read MONTH
+echo "3. Please type in the two digit day you were born in followed by [Enter]:"
+read DAY
+echo "4. Please type in the city you were born in followed by [Enter]:"
+read CITY
+echo "5. Please type in the state you were born in followed by [Enter]:"
+read STATE
+INPUT_DATE=`date -I`
 
+echo "$YEAR, $MONTH, $DAY, $CITY, $STATE, $INPUT_DATE"
